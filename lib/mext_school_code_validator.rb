342ea79485c1afcaab3753e8731bc87b28eddf27
Seiei Miyagi <hanachin@gmail.com>
2023-03-30 22:28:37 +0900

    bundle gem mext_school_code_validator

---
+++ lib/mext_school_code_validator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mext_school_code_validator/version"
+
+module MextSchoolCodeValidator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mext_school_code_validator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MextSchoolCodeValidator do
+  it "has a version number" do
+    expect(MextSchoolCodeValidator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
