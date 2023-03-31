3b33344f65585ae2625029e8e5a6410529adca32
Seiei Miyagi <hanachin@gmail.com>
2023-03-31 09:47:23 +0900

    Update validation

--- lib/mext_school_code_validator.rb
+++ lib/mext_school_code_validator.rb
@@ -10,21 +10,21 @@ class MextSchoolCodeValidator < ActiveModel::EachValidator
     "A" => "01", "B" => "02", "C" => "03", "D" => "04", "E" => "05",
     "F" => "06", "G" => "07", "H" => "08"
   }.freeze
-  VALID_ORG_TYPE_REGEXP = /[123]/.freeze
-  VALID_PREFECTURE_NO_REGEXP = /..(?:0[1-9]|(?:[1-3][0-9])|(?:4[0-7]))/.freeze
-  VALID_SCHOOL_NO_REGEXP = /[1-9][0-9]{6}/.freeze
-  VALID_SCHOOL_TYPE_REGEXP = /A1|A2|B1|C1|C2|D1|D2|E1|F1|F2|G1|H1|H2/.freeze
+  VALID_ORG_TYPE_REGEXP = /\A[123]\z/.freeze
+  VALID_PREFECTURE_NO_REGEXP = /\A(?:0[1-9]|(?:[1-3][0-9])|(?:4[0-7]))\z/.freeze
+  VALID_SCHOOL_NO_REGEXP = /\A[1-9][0-9]{6}\z/.freeze
+  VALID_SCHOOL_TYPE_REGEXP = /\A(?:A1|A2|B1|C1|C2|D1|D2|E1|F1|F2|G1|H1|H2)/.freeze
 
   def validate_each(record, attribute, value) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
     value = value.presence.to_s
 
     if value.length != LENGTH
       record.errors.add(attribute, (options[:message] || :wrong_length), count: LENGTH)
-    elsif !value.start_with?(VALID_SCHOOL_TYPE_REGEXP)
+    elsif !value.match?(VALID_SCHOOL_TYPE_REGEXP)
       school_type = value[0, 2]
       message = (options[:message] || :mext_school_code_invalid_school_type)
       record.errors.add(attribute, message, school_type: school_type)
-    elsif !value.start_with?(VALID_PREFECTURE_NO_REGEXP)
+    elsif !value[2, 2].match?(VALID_PREFECTURE_NO_REGEXP)
       prefecture_no = value[2, 2]
       message = (options[:message] || :mext_school_code_invalid_prefecture_no)
       record.errors.add(attribute, message, prefecture_no: prefecture_no)
